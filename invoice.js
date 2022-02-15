782d7e4f44f5d163676e130dff60758b0256e5ba
bideeen <b.abideen@yahoo.com>
2022-02-15 22:01:51 +0100

    2.0

--- invoice.js
+++ invoice.js
@@ -1,45 +1,67 @@
 const car = document.getElementById('car')
 const lawn = document.getElementById('lawn')
 const weeds = document.getElementById('weeds')
+const reset = document.getElementById('reset')
 let task = document.getElementById('task')
+let c_k = 1
+let l_k = 1
+let w_k = 1
+
 
 
 let invoice = []
 let total = 0
 
-
+// Car button Action
 car.addEventListener('click', function(){
-    invoice.push(10)
-    total += 10
-    Invoice()
-}, {once:true})
+    if (c_k === 1) {
+        invoice.push(`Wash Car <span style="float:right;">$10</span><br>`)
+        total += 10
+        Invoice()
+        c_k = c_k - c_k
+    }
+})
+// Lawn button Action
 lawn.addEventListener('click', function(){
-    invoice.push(20)
-    total += 20
-    Invoice()
-}, {once:true})
+    if (l_k === 1) {
+        invoice.push(`Mow Lawn <span style="float:right;">$20</span><br>`)
+        total += 20
+        Invoice()
+        l_k = l_k - l_k
+    }
+})
+// Weeds button Action
 weeds.addEventListener('click', function(){
-    invoice.push(30)
-    total += 30
-    Invoice()
-}, {once:true})
+    if (w_k === 1) {
+        invoice.push(`Pull Weeds <span style="float:right;">$30</span><br>`)
+        total += 30
+        Invoice()
+        w_k = w_k - w_k
+    }
+})
+
+reset.addEventListener('click', function(){
+    invoice = []
+    task.innerHTML = ''
+    for (let i = 0; i < invoice.length; i++) {
+        task.innerHTML += invoice[i]
+    }
+    total = 0
+    document.getElementById('total').innerHTML =`<span style="float:right;">$${total}</span>`
+    c_k = 1
+    l_k = 1
+    w_k = 1    
+     
+})
+
 
 function Invoice(){
-
+    task.innerHTML = ''
     for (let i = 0; i < invoice.length; i++) {
-        if ( invoice[i] === 10){
-            task.innerHTML = `Wash Car <span style="float:right;">$${invoice[i]}</span>`
-        }
-        else if ( invoice[i] === 20){
-            task.innerHTML = `Mow Lawn <span style="float:right;">$${invoice[i]}</span>`
-        }
-        else if ( invoice[i] === 30){
-            task.innerHTML = `Pull Weeds <span style="float:right;">$${invoice[i]}</span>`
-        }
-    }
-    
+        task.innerHTML += invoice[i]
+    }   
 
-    document.getElementById('total').textContent = "$" + total
+    document.getElementById('total').innerHTML =`We accept card, Credit card or Paypal. <span style="float:right;">$${total}</span>`
 }
 
 
